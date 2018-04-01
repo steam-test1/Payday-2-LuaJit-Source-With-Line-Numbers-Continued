a1d85aedb673128849eea0581fd102f155bce355
TdlQ <TdlQ@localhost>
2018-04-01 17:56:19 +0200

    v1.89.554 (U4118)

--- lib/managers/dlc/dlcmanagergenerateddata.lua
+++ lib/managers/dlc/dlcmanagergenerateddata.lua
@@ -1,5 +1,8 @@
 
--- Lines: 13 to 14
+-- Lines: 9 to 14
 function WINDLCManager:init_generated()
+	Global.dlc_manager.all_dlc_data.toon = {}
+	Global.dlc_manager.all_dlc_data.toon.app_id = "218620"
+	Global.dlc_manager.all_dlc_data.toon.no_install = true
 end
 
--- lib/tweak_data/generateddlctweakdata.lua
+++ lib/tweak_data/generateddlctweakdata.lua
@@ -1,5 +1,32 @@
 
--- Lines: 23 to 24
+-- Lines: 12 to 24
 function DLCTweakData:init_generated()
+	self.toon_bundle = {
+		free = true,
+		content = {}
+	}
+	self.toon_bundle.content.loot_global_value = "toon"
+	self.toon_bundle.content.loot_drops = {
+		{
+			type_items = "masks",
+			item_entry = "toon_01",
+			amount = 1
+		},
+		{
+			type_items = "masks",
+			item_entry = "toon_02",
+			amount = 1
+		},
+		{
+			type_items = "masks",
+			item_entry = "toon_03",
+			amount = 1
+		},
+		{
+			type_items = "masks",
+			item_entry = "toon_04",
+			amount = 1
+		}
+	}
 end
 
