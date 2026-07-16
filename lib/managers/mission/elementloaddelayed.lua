eca1c66098b9c99a47842941b9e34c64619a3fdc
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2026-07-16 15:40:24 +0100

    Open Beta 4

--- lib/managers/mission/elementloaddelayed.lua
+++ lib/managers/mission/elementloaddelayed.lua
@@ -2,17 +2,17 @@ core:import("CoreMissionScriptElement")
 
 ElementLoadDelayed = ElementLoadDelayed or class(CoreMissionScriptElement.MissionScriptElement)
 
--- Lines 5-7
+-- Lines 9-11
 function ElementLoadDelayed:init(...)
 	ElementLoadDelayed.super.init(self, ...)
 end
 
--- Lines 9-11
+-- Lines 13-15
 function ElementLoadDelayed:client_on_executed(...)
 	self:on_executed(...)
 end
 
--- Lines 13-22
+-- Lines 17-26
 function ElementLoadDelayed:on_executed(instigator)
 	if not self._values.enabled then
 		return
@@ -25,21 +25,30 @@ function ElementLoadDelayed:on_executed(instigator)
 	ElementLoadDelayed.super.on_executed(self, instigator)
 end
 
--- Lines 24-28
+-- Lines 28-37
 function ElementLoadDelayed:_create_units()
+	if self._created_units then
+		Application:stack_dump_error("[ElementLoadDelayed:_create_units] Attempted to spawn units again!", self._id, self._editor_name)
+
+		return
+	end
+
 	self._created_units = true
 
 	self._mission_script:add_save_state_cb(self._id)
 	managers.worlddefinition:create_delayed_unit(self._values.unit_ids)
 end
 
--- Lines 30-36
+-- Lines 39-47
 function ElementLoadDelayed:save(data)
+	data.enabled = self._values.enabled
 	data.created_units = self._created_units
 end
 
--- Lines 38-47
+-- Lines 49-61
 function ElementLoadDelayed:load(data)
+	self:set_enabled(data.enabled)
+
 	if data.created_units then
 		self:_create_units()
 	end
